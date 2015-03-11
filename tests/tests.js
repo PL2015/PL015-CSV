a65826f6f80044a283739de157e8b840691d64f7
Wyllman <wyllman@gmail.com>
2015-03-11 19:26:20 +0000

    Ampliando la estructura de las pruebas. Archivo tests/test.js

---
+++ tests/tests.js
@@ -0,0 +1,13 @@
+var assert = chai.assert;
+
+suite('csv', function() {
+    test('Primera prueba', function() {
+        
+    });
+    test('Segunda prueba', function() {
+        
+    });
+    test('Tercera prueba', function() {
+        
+    });
+});
