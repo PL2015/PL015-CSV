086a15eb57d7e9d587a2dc8042748b199205e5b1
Wyllman <wyllman@gmail.com>
2015-03-11 20:12:36 +0000

    Añadido blanket para el encubrimiento

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,8 @@ var assert = chai.assert;
 
 suite('csv', function() {
     test('Primera prueba', function() {
-        
+        var a = "Hola";
+        assert.equal(a, "Hola", 'El saludo');
     });
     test('Segunda prueba', function() {
         
