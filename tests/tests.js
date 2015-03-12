990cc4c7cc37fb69de56f771d14e8b490167b6a9
Wyllman <wyllman@gmail.com>
2015-03-12 01:04:34 +0000

    Creada la primera prueba real.

--- tests/tests.js
+++ tests/tests.js
@@ -4,17 +4,29 @@ suite('csv', function() {
     setup(function(){
         if (typeof __html__ !== 'undefined') {
             document.body.innerHTML = __html__['test.html'];
-            //original = document.getElementById('original');
+            original = document.getElementById("original");
             //finaltable = document.getElementById('finaltable');
+            //result = document.getElementById('result');
         }
+        
     });
 
     test('Primera prueba', function() {
         var a = "Hola";
-        assert.equal(a, "Hola", 'El saludo');
+        var result;
+        //var original = document.getElementById("original");
+        
+        original.value = '1';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML, '\n<tbody><tr> <td>1</td> </tr>\n</tbody>', 'Elemento unico.');
     });
+    
+    
     test('Segunda prueba', function() {
         var b = "Hola 2";
-        assert.equal(b, "Hola 3", 'El segundo saludo');
+        assert.equal(b, "Hola 2", 'El segundo saludo');
     });
 });
