b32ac64f69d6aa8c808c0ed3439d38e22a2fe09a
Wyllman <wyllman@gmail.com>
2015-03-12 01:30:47 +0000

    Arreglos menores en package.json y test.html.(2)

--- tests/tests.js
+++ tests/tests.js
@@ -1,32 +1,109 @@
 var assert = chai.assert;
 
-suite('csv', function() {
+suite('CSV - Creando las tablas ...', function() {
     setup(function(){
         if (typeof __html__ !== 'undefined') {
             document.body.innerHTML = __html__['test.html'];
             original = document.getElementById("original");
-            //finaltable = document.getElementById('finaltable');
-            //result = document.getElementById('result');
         }
         
     });
 
-    test('Primera prueba', function() {
-        var a = "Hola";
+    test('Primera prueba: Creando tablas con una fila.', function() {
         var result;
-        //var original = document.getElementById("original");
         
         original.value = '1';
         calculate();
         
         result = document.getElementById('result');
         
-        assert.deepEqual(result.innerHTML, '\n<tbody><tr> <td>1</td> </tr>\n</tbody>', 'Elemento unico.');
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td> </tr>\n</tbody>'
+                        , 'Elemento unico.');
+        
+        original.value = '1, 2';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td>  <td> 2</td> </tr>\n</tbody>'
+                        , 'Dos elementos.');
+        
     });
     
     
-    test('Segunda prueba', function() {
-        var b = "Hola 2";
-        assert.equal(b, "Hola 2", 'El segundo saludo');
+    test('Segunda prueba: Creando tablas con varias filas.', function() {
+        original.value = '1\n2';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td> </tr>\n<tr> <td>2</td> </tr>\n</tbody>'
+                        , 'Elemento unico.');
+        
+        original.value = '1, 2\n3, 4';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td>  <td> 2</td> </tr>\n<tr> <td>3</td>  <td> 4</td> </tr>\n</tbody>'
+                        , 'Dos elementos.');
+    });
+});
+
+suite('CSV - Comprobando funcionalidades ...', function() {
+    setup(function(){
+        if (typeof __html__ !== 'undefined') {
+            document.body.innerHTML = __html__['test.html'];
+            original = document.getElementById("original");
+        }
+        
+    });
+
+    test('Tercera prueba: Comprobando el uso de localStorage.', function() {
+        var result;
+        
+        original.value = '1';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td> </tr>\n</tbody>'
+                        , 'Elemento unico.');
+        
+        original.value = '1, 2';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td>  <td> 2</td> </tr>\n</tbody>'
+                        , 'Dos elementos.');
+        
+    });
+    
+    
+    test('Segunda prueba: Creando tablas con varias filas.', function() {
+        original.value = '1\n2';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td> </tr>\n<tr> <td>2</td> </tr>\n</tbody>'
+                        , 'Elemento unico.');
+        
+        original.value = '1, 2\n3, 4';
+        calculate();
+        
+        result = document.getElementById('result');
+        
+        assert.deepEqual(result.innerHTML
+                        , '\n<tbody><tr> <td>1</td>  <td> 2</td> </tr>\n<tr> <td>3</td>  <td> 4</td> </tr>\n</tbody>'
+                        , 'Dos elementos.');
     });
 });
